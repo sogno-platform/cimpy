5f24cfaf2abf647bd3e6a7bf4ee4f90e79bbc6ec
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2020-04-09 13:59:53 +0200

    fix setup package list

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
    name = 'cimpy',
-   version = '0.9.2',
+   version = '0.9.3',
    description = 'Python package for import, modification and export of CIM grid data',
    author = 'Institute for Automation of Complex Power Systems',
    author_email = 'acs-software@eonerc.rwth-aachen.de',
    license = 'MPL-2.0',
-   packages = [ 'cimpy' ],
+   packages = setuptools.find_packages(),
    install_requires = [
        'lxml',
        'xmltodict',
