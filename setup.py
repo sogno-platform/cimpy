7175d365c82f062808576a50f8658fb23ba9af06
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2020-06-30 18:21:55 +0200

    bump version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
    name = 'cimpy',
-   version = '0.9.4',
+   version = '1.0.0',
    description = 'Python package for import, modification and export of CIM grid data',
    author = 'Institute for Automation of Complex Power Systems',
    author_email = 'acs-software@eonerc.rwth-aachen.de',
