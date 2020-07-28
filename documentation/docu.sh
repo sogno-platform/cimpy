84de61d3adfa60898237780d71b8138e18a96acc
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2020-07-28 12:37:08 +0200

    Removed duplicate docu script. Fixes #22

--- documentation/docu.sh
+++ documentation/docu.sh
@@ -1,5 +1,5 @@
 python3 ../setup.py develop
-sphinx-apidoc -F -f -H "cimpy" -o "../documentation-build" "../"
+sphinx-apidoc -F -H "cimpy" -o "../documentation" "../"
 python3 set_inheritance_diagram.py
-cd ../documentation-build
+cd ../documentation
 make html
