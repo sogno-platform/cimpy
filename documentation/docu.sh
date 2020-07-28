d35e043d2fde3ed70a5608cee3e95381b3b41f1e
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2020-07-28 13:10:10 +0200

    removed set_inheritance_diagram.py as it seems broken

--- documentation/docu.sh
+++ documentation/docu.sh
@@ -1,5 +1,6 @@
 python3 ../setup.py develop
 sphinx-apidoc -F -H "cimpy" -o "../documentation" "../"
-python3 set_inheritance_diagram.py
+# This is broken
+# python3 set_inheritance_diagram.py
 cd ../documentation
 make html
