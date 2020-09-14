714f37d062b280fe4f8499428dc9d02b10b9435f
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2020-09-14 13:33:29 +0200

    Excluded setup.py from the documentation

--- documentation/docu.sh
+++ documentation/docu.sh
@@ -1,5 +1,5 @@
 python3 ../setup.py develop
-sphinx-apidoc -F -H "cimpy" -o "../documentation" "../"
+sphinx-apidoc -F -H "cimpy" -o "../documentation" "../" "../setup.py"
 python3 set_inheritance_diagram.py
 cd ../documentation
 make html
