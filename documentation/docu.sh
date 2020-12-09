74a59bc89b995af937d2f3c5248f27cda6e4c03a
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2020-12-09 22:54:00 +0100

    removed cache and use text build for dev branches instead

--- documentation/docu.sh
+++ documentation/docu.sh
@@ -1,5 +1,12 @@
-python3 ../setup.py develop
-sphinx-apidoc -F -H "cimpy" --separate -o "../documentation" "../" "../setup.py"
-python3 set_inheritance_diagram.py
-cd ../documentation
-make html
+if [ "$1" = "--release" ] || [ "$1" == "" ]; then
+    python3 ../setup.py develop
+    sphinx-apidoc -F -H "cimpy" --separate -o "../documentation" "../" "../setup.py"
+    python3 set_inheritance_diagram.py
+    if [ "$1" = "--release" ] ; then
+        make html
+    else
+        make text
+    fi
+else
+    echo "Usage: $0 [--release]"
+fi
