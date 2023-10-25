9c86e407ea1227c28d7078cf108a47ee1b88a29d
Canfirat98 <can.yaruk@rwth-aachen.de>
2023-10-25 14:47:54 +0200

    rename file utils.py
    Signed-off-by: Canfirat98 <can.yaruk@rwth-aachen.de>

--- cimpy/__init__.py
+++ cimpy/__init__.py
@@ -1,6 +1,6 @@
 from cimpy.cimexport import cim_export
 from cimpy.cimimport import cim_import
-import cimpy.utils
+import cimpy.cimedit
 from cimpy.cimexamples import import_example
 from cimpy.cimexamples import export_example
 from cimpy.cimexamples import addExternalNetworkInjection_example
