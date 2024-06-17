5caeeead29378293dca491e5c42e9f10c5dcd340
Steffen Vogel <steffen.vogel@opal-rt.com>
2024-06-17 17:32:52 +0200

    Simplify CI action for building docs
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -n "${GITHUB_WORKSPACE}" ];
-then
-    cd documentation
-    ./docu.sh --release
-    cp -a ${GITHUB_WORKSPACE}/documentation/_build/html ${GITHUB_WORKSPACE}/built_documentation
-fi
